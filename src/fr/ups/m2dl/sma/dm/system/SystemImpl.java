ff669d4eb4294b5f573ab800973b24c0034a9b7e
kserin <lepiaf31@gmail.com>
2014-06-08 16:24:25 +0200

    #12 creation of the system component

---
+++ src/fr/ups/m2dl/sma/dm/system/SystemImpl.java
@@ -0,0 +1,79 @@
+/**
+ * 
+ */
+package fr.ups.m2dl.sma.dm.system;
+
+import fr.ups.m2dl.sma.dm.system.agents.RobotEcosystemImpl;
+import fr.ups.m2dl.sma.dm.system.agents.RobotsEcosystem;
+import fr.ups.m2dl.sma.dm.system.components.configuration.Configurator;
+import fr.ups.m2dl.sma.dm.system.components.environment.EnvironmentRepresentation;
+import fr.ups.m2dl.sma.dm.system.components.log.Printer;
+import fr.ups.m2dl.sma.dm.system.components.network.NetworkCycleManager;
+import fr.ups.m2dl.sma.dm.system.components.process.Controller;
+import fr.ups.m2dl.sma.dm.system.components.process.CycleManager;
+import fr.ups.m2dl.sma.dm.system.components.system.System;
+import fr.ups.m2dl.sma.dm.system.configuration.ConfiguratorImpl;
+import fr.ups.m2dl.sma.dm.system.environment.EnvironmentRepresentationImpl;
+import fr.ups.m2dl.sma.dm.system.log.ILog;
+import fr.ups.m2dl.sma.dm.system.log.PrinterDispatcherImpl;
+import fr.ups.m2dl.sma.dm.system.log.SimplePrinterImpl;
+import fr.ups.m2dl.sma.dm.system.network.DummyNetworkCycleManagerImpl;
+import fr.ups.m2dl.sma.dm.system.process.ControllerImpl;
+import fr.ups.m2dl.sma.dm.system.process.CycleDispatcherImpl;
+import fr.ups.m2dl.sma.dm.system.process.CycleManagerImpl;
+import fr.ups.m2dl.sma.dm.system.process.ICycle;
+import fr.ups.m2dl.sma.dm.utils.Dispatcher2;
+import fr.ups.m2dl.sma.dm.utils.Dispatcher3;
+
+/**
+ * @author SERIN Kevin
+ *
+ */
+public class SystemImpl extends System {
+
+	@Override
+	protected NetworkCycleManager make_networkManager() {
+		return new DummyNetworkCycleManagerImpl();
+	}
+
+	@Override
+	protected EnvironmentRepresentation make_environment() {
+		return new EnvironmentRepresentationImpl();
+	}
+
+	@Override
+	protected RobotsEcosystem make_eco() {
+		return new RobotEcosystemImpl();
+	}
+
+	@Override
+	protected Dispatcher2<ILog> make_logDispatcher() {
+		return new PrinterDispatcherImpl();
+	}
+
+	@Override
+	protected Printer make_printer() {
+		return new SimplePrinterImpl();
+	}
+
+	@Override
+	protected Dispatcher3<ICycle> make_cycleDispatcher() {
+		return new CycleDispatcherImpl();
+	}
+
+	@Override
+	protected CycleManager make_cycleManager() {
+		return new CycleManagerImpl();
+	}
+	
+	@Override
+	protected Controller make_controller() {
+		return new ControllerImpl();
+	}
+
+	@Override
+	protected Configurator make_configurator() {
+		return new ConfiguratorImpl();
+	}
+
+}
--- src/fr/ups/m2dl/sma/dm/system/process/CycleDispatcherImpl.java
+++ src/fr/ups/m2dl/sma/dm/system/process/CycleDispatcherImpl.java
@@ -3,13 +3,13 @@
  */
 package fr.ups.m2dl.sma.dm.system.process;
 
-import fr.ups.m2dl.sma.dm.utils.Dispatcher2;
+import fr.ups.m2dl.sma.dm.utils.Dispatcher3;
 
 /**
  * @author SERIN Kevin
  *
  */
-public class CycleDispatcherImpl extends Dispatcher2<ICycle> {
+public class CycleDispatcherImpl extends Dispatcher3<ICycle> {
 
 	@Override
 	protected ICycle make_from() {
@@ -19,12 +19,14 @@ public class CycleDispatcherImpl extends Dispatcher2<ICycle> {
 			public void doCyle() {
 				requires().to1().doCyle();
 				requires().to2().doCyle();
+				requires().to3().doCyle();
 			}
 			
 			@Override
 			public void afterCycle() {
 				requires().to1().afterCycle();
 				requires().to2().afterCycle();
+				requires().to3().afterCycle();
 			}
 		};
 	}
